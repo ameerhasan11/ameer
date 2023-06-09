8a2b456ae2cd8e7d966a50cce307fc9738e65f59
ameerhasan11 <136092290+ameerhasan11@users.noreply.github.com>
2023-06-09 19:58:53 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+     <div className="a">
+     <div className='ff'>4th Floor</div>
+
+      <div className='x'>     <div className='we'>Non AC</div> <p className='p'>Jamesh Wood</p>  <p className='b'>Wordey Limpi</p>  <p className='l'>soumya Khanna</p> <p className='m'> Liza Doolittle </p> <p className='n'>FF - 148</p> <p className='f'>FF - 149</p>  <p className='v'>FF - 150</p>  <p className='q'>FF - 151</p>  <p className='o'>FF - 152</p> <p className='c'>FF - 153</p> <p className='d'>FF - 154</p> <p className='e'>FF - 155</p></div>
+     </div>
+
+     <div className="w">
+     <div className='ff'>3th Floor</div>
+     <div></div>
+      <div className='x'><div className='wd'>Private Ward</div> <p className='ss'>Mukul Jain</p>  <p className='sa'>Mukul singh</p> <p className='sb'>TF - 103</p> <p className='sc'>Madhu</p></div>
+     <div></div>
+      <div className='x'><div className='wb'>General Ward </div><p className='sq'>Arnav Joshi</p>  <p className='sn'>TF - 110</p> <p className='sl'>Wilson Burton</p> <p className='sh'>varun mahajan</p> <p className='sk'>Piyus Jain</p>  <p className='sf'> Emma Watson</p> </div>
+     </div>
+
+     <div className="s">
+     <div className='ff'>2th Floor</div>
+     <div></div>
+      <div className='x'><div className='we'>ICU</div><p className='as'>Cameron Martin</p> <p className='ac'>Harry Thomash</p> <p className='ah'>SF - 138</p><p className='ad'>Gaurav Shrivasta...</p> <p className='aq'>Jennifer  D'souza</p></div>
+     <div></div>
+      <div className='x'><div className='we'>NICU</div><p className='sq'>Pratik gandhi</p> <p className='cx'>Dhawan Kulkarni</p> <p className='cq'>Tarun Khanna</p> <p className='cv'>SF - 136 </p> <p className='cp'>SF - 135 </p></div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
